f1df860e3c8f0eaca34fbe4ec86e2736eed075be
박태률 <taeryul0211@gmail.com>
2025-12-07 21:58:29 +0900

    realreal

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def profile():
 #     return jsonify({"arduino_data": line})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
